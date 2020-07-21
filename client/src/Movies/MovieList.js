5932b7ff309a402d3a1a4caea2554f218f98b1dd
Leonard Slater <slaterboy89@gmail.com>
2020-07-21 00:00:40 -0400

    finished

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom'
 const MovieList = props => {
   return (
     <div className="movie-list">
@@ -14,6 +14,7 @@ function MovieDetails({ movie }) {
   const { title, director, metascore } = movie;
   return (
     <div className="movie-card">
+    <Link to= {`/movies/${movie.id}`}>
       <h2>{title}</h2>
       <div className="movie-director">
         Director: <em>{director}</em>
@@ -21,6 +22,7 @@ function MovieDetails({ movie }) {
       <div className="movie-metascore">
         Metascore: <strong>{metascore}</strong>
       </div>
+    </Link>
     </div>
   );
 }
--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,12 +1,14 @@
 import React from 'react';
-
+import {Link} from 'react-router-dom'
 const SavedList = props => (
   <div className="saved-list">
     <h3>Saved Movies:</h3>
     {props.list.map(movie => (
       <span className="saved-movie">{movie.title}</span>
     ))}
-    <div className="home-button">Home</div>
+    <div className="home-button">
+      <Link to= '/'>Home</Link>
+      </div>
   </div>
 );
 
